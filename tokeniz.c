72d8323a46bc156cd629de0a3585b82cfe24dbbe
McOuma <oumamugah@gmail.com>
2023-09-20 09:47:17 +0300

    main files

--- tokeniz.c
+++ tokeniz.c
@@ -1,5 +1,4 @@
 #include "och_wand.h"
-
 /**
  * wand_propmt - displays the prompt for user to input.
  *
@@ -7,23 +6,22 @@
  */
 void tokenize_command(char *command, char *args[])
 {
-	char *token;
-	int i = 0;
-
-	if (command[0] == '\0')
-	{
-		return;
-	}
-	token = strtok(command, " ");
-	while (token != NULL && i < COMMAND_LENGTH - 1)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
-	if (strcmp(args[0], "exit") == 0)
-	{
-		exit(0);
-	}
-	execute_command(args);
+char *token;
+int i = 0;
+if (command[0] == '\0')
+{
+return;
+}
+token = strtok(command, " ");
+while (token != NULL && i < COMMAND_LENGTH - 1)
+{
+args[i++] = token;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
+if (strcmp(args[0], "exit") == 0)
+{
+exit(0);
+}
+execute_command(args);
 }
